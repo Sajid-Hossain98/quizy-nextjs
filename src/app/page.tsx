b1d051c1641d9ecc7f7d19b0bfe9384acacd161f
Sajid Hossain <bmsajidhossain420@gmail.com>
2023-07-31 23:31:00 +0600

    protected route for homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import SignInButton from "@/components/SignInButton";
-import { Button } from "@/components/ui/button";
+import { redirect } from "next/navigation";
 import {
   Card,
   CardContent,
@@ -7,8 +7,14 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { getAuthSession } from "@/lib/nextauth";
 
 export default async function Home() {
+  const session = await getAuthSession();
+  if (session?.user) {
+    //means that the user is logged in
+    return redirect("/dashboard");
+  }
   return (
     <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
       <Card className="md:w-[500px] w-[300px]">
